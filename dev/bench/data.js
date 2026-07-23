8514791d073f568506903304e556f1d8d68635a1
github-action-benchmark <github@users.noreply.github.com>
2026-07-23 19:41:59 +0000

    add Screen FPS (customBiggerIsBetter) benchmark result for 46357cfc28fdf3ce712dde6242bdb0b467aac6f7

---
+++ dev/bench/data.js
@@ -0,0 +1,65 @@
+window.BENCHMARK_DATA = {
+  "lastUpdate": 1784835719517,
+  "repoUrl": "https://github.com/AntoC-dev/Recipedia",
+  "entries": {
+    "Screen FPS": [
+      {
+        "commit": {
+          "author": {
+            "name": "semantic-release-bot",
+            "username": "semantic-release-bot",
+            "email": "semantic-release-bot@martynus.net"
+          },
+          "committer": {
+            "name": "semantic-release-bot",
+            "username": "semantic-release-bot",
+            "email": "semantic-release-bot@martynus.net"
+          },
+          "id": "46357cfc28fdf3ce712dde6242bdb0b467aac6f7",
+          "message": "chore(release): 2.46.1 [skip ci]\n\n## [2.46.1](https://github.com/AntoC-dev/Recipedia/compare/v2.46.0...v2.46.1) (2026-07-23)\n\n### Bug Fixes\n\n* **#305:** satisfy strict types in shopping reset tests ([4fc79d4](https://github.com/AntoC-dev/Recipedia/commit/4fc79d46f596e97c6dbdcf3c40d2d6fac8035c4b)), closes [#305](https://github.com/AntoC-dev/Recipedia/issues/305)\n* reset purchased items after final cook ([ca3b589](https://github.com/AntoC-dev/Recipedia/commit/ca3b58964d89e4910f700ccbdcc892d6dc6b5fdc))\n* reset purchased items after final cook ([#390](https://github.com/AntoC-dev/Recipedia/issues/390)) ([9877632](https://github.com/AntoC-dev/Recipedia/commit/98776322f9208d25a6752f54d5bd9b4dd58b134e))",
+          "timestamp": "2026-07-23T17:47:33Z",
+          "url": "https://github.com/AntoC-dev/Recipedia/commit/46357cfc28fdf3ce712dde6242bdb0b467aac6f7"
+        },
+        "date": 1784835717807,
+        "tool": "customBiggerIsBetter",
+        "benches": [
+          {
+            "name": "00_seed",
+            "value": 59.3,
+            "unit": "fps"
+          },
+          {
+            "name": "01_app_start",
+            "value": 59.5,
+            "unit": "fps"
+          },
+          {
+            "name": "02_home",
+            "value": 59.2,
+            "unit": "fps"
+          },
+          {
+            "name": "03_search",
+            "value": 58.5,
+            "unit": "fps"
+          },
+          {
+            "name": "04_recipe_view",
+            "value": 58.7,
+            "unit": "fps"
+          },
+          {
+            "name": "05_shopping",
+            "value": 58.7,
+            "unit": "fps"
+          },
+          {
+            "name": "06_parameters",
+            "value": 55.2,
+            "unit": "fps"
+          }
+        ]
+      }
+    ]
+  }
+}
